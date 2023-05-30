ebc155f3b5915824bc42f2e217a99fa8666f2853
GFier <guido@studiofreight.com>
2023-05-30 23:15:02 +0100

    add accordion

---
+++ src/accordion/index.js
@@ -0,0 +1,58 @@
+import * as RadixAccordion from '@radix-ui/react-accordion'
+import cn from 'clsx'
+import { forwardRef } from 'react'
+import s from './accordion.module.scss'
+
+export function Accordion({ children, className, ...props }) {
+  return (
+    <div className={cn(s.container, className)}>
+      <RadixAccordion.Root {...props}>{children}</RadixAccordion.Root>
+    </div>
+  )
+}
+
+export const AccordionItem = forwardRef(
+  ({ children, value, className = '' }, ref) => (
+    <RadixAccordion.Item
+      ref={ref}
+      value={value}
+      className={cn(className, s['item-wrapper'])}
+    >
+      {children}
+    </RadixAccordion.Item>
+  )
+)
+
+export const AccordionTrigger = forwardRef(
+  ({ children, className = '' }, ref) => (
+    <RadixAccordion.Header>
+      <RadixAccordion.Trigger
+        className={cn(className, s['trigger-wrapper'])}
+        ref={ref}
+      >
+        {children}
+      </RadixAccordion.Trigger>
+    </RadixAccordion.Header>
+  )
+)
+
+export const AccordionContent = forwardRef(
+  ({ children, className = '' }, ref) => {
+    return (
+      <RadixAccordion.Content
+        className={cn(className, s['content-wrapper'])}
+        ref={ref}
+      >
+        {children}
+      </RadixAccordion.Content>
+    )
+  }
+)
+
+AccordionItem.displayName = 'AccordionItem'
+AccordionTrigger.displayName = 'AccordionTrigger'
+AccordionContent.displayName = 'AccordionContent'
+
+Accordion.Trigger = AccordionTrigger
+Accordion.Content = AccordionContent
+Accordion.Item = AccordionItem
