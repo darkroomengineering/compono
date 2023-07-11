11bdb92962d485797df28168fb15325ad56d811e
arzafran <franco@studiofreight.com>
2023-07-11 17:01:53 -0300

    remove lottie file

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -1,15 +1,11 @@
-import { Lottie } from '@studio-freight/compono'
 import { Layout } from 'layouts/default'
 import s from './home.module.scss'
-
-import LottieFile from '/public/lotties/animation.json'
 
 export default function Home() {
   return (
     <Layout theme="light">
       <section className={s.content}>
         <p>hi</p>
-        <Lottie animation={LottieFile} loop />
       </section>
     </Layout>
   )
