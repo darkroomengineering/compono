55650b138f686e66345567c430bc645cc240764e
Clément Roche <rchclement@gmail.com>
2023-07-12 15:37:56 +0200

    clean link

--- src/link/index.js
+++ src/link/index.js
@@ -1,42 +1,42 @@
 import NextLink from 'next/link'
-import { forwardRef, useMemo } from 'react'
+import { useRouter } from 'next/router'
+import { forwardRef } from 'react'
 
 const SHALLOW_URLS = ['?demo=true']
 
-export const Link = forwardRef(({ href, children, className, scroll, shallow, ...props }, ref) => {
-  const attributes = {
-    ref,
-    className,
-    ...props,
-  }
-
-  const isProtocol = useMemo(() => href?.startsWith('mailto:') || href?.startsWith('tel:'), [href])
-
-  const needsShallow = useMemo(() => !!SHALLOW_URLS.find((url) => href?.includes(url)), [href])
-
-  const isAnchor = useMemo(() => href?.startsWith('#'), [href])
-  const isExternal = useMemo(() => href?.startsWith('http'), [href])
+export const Link = forwardRef(({ href, children, shallow, scroll, fallback = 'div', ...props }, ref) => {
+  const router = useRouter()
 
   if (typeof href !== 'string') {
-    return <button {...attributes}>{children}</button>
-  }
+    const Tag = fallback
 
-  if (isProtocol || isExternal) {
     return (
-      <a {...attributes} href={href} target="_blank" rel="noopener noreferrer">
+      <Tag ref={ref} {...props}>
         {children}
-      </a>
+      </Tag>
     )
   }
 
+  const isExternal = href?.startsWith('http')
+  const isProtocol = href?.startsWith('mailto:') || href?.startsWith('tel:')
+  const needsShallow = !!SHALLOW_URLS.find((url) => href?.includes(url))
+  const isSamePath = router?.pathname === href
+
   return (
     <NextLink
+      ref={ref}
       href={href}
-      passHref={isAnchor}
       shallow={needsShallow || shallow}
       scroll={scroll}
-      {...attributes}
-      {...(isExternal && { target: '_blank', rel: 'noopener noreferrer' })}
+      {...((isProtocol || isExternal) && {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      })}
+      onClick={(e) => {
+        if (isSamePath) e.preventDefault()
+        props?.onClick?.()
+      }}
+      {...props}
     >
       {children}
     </NextLink>
