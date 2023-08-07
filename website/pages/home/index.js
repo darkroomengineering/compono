50bef65ae9b7f4b7b4a99665c5234a258a123c98
arzafran <franco@studiofreight.com>
2023-08-07 17:28:06 -0300

    update dependencies, use latest for ours and nextjs

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -1,11 +1,29 @@
 import { Layout } from 'layouts/default'
+import { Accordion } from '../../../src'
 import s from './home.module.scss'
 
 export default function Home() {
   return (
     <Layout theme="light">
       <section className={s.content}>
-        <p>hi</p>
+        <Accordion type="single" collapsible={true} className={s.accordion}>
+          {[{ trigger: 'click me', content: 'show me' }].map(
+            ({ trigger, content }, idx) => (
+              <Accordion.Item
+                key={`accordion-item-${idx}`}
+                value={`accordion-item-${idx}`}
+                className={s.item}
+              >
+                <Accordion.Trigger className={s.trigger}>
+                  <p>{trigger}</p>
+                </Accordion.Trigger>
+                <Accordion.Content className={s.content}>
+                  <p>{content}</p>
+                </Accordion.Content>
+              </Accordion.Item>
+            ),
+          )}
+        </Accordion>
       </section>
     </Layout>
   )
