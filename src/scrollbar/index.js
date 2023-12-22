a051f18832b36da2c498a72271df5ab53945179d
Clément Roche <rchclement@gmail.com>
2023-12-22 14:35:28 +0100

    packages version

--- src/scrollbar/index.js
+++ src/scrollbar/index.js
@@ -1,88 +1,72 @@
-import { useRect, useWindowSize } from '@studio-freight/hamo'
+import { useRect } from '@studio-freight/hamo'
 import { useLenis } from '@studio-freight/react-lenis'
+import { mapRange } from 'libs/maths'
 import { useEffect, useRef, useState } from 'react'
-import { clamp, mapRange } from '../../lib/maths'
 import s from './scrollbar.module.scss'
 
-export function Scrollbar({
-  theme = {
-    color: '#ff0000',
-    borderColor: '#fff',
-    borderSize: '1px',
-    borderRadius: '4px',
-    minHeight: '48px',
-    positionRight: '10px',
-  },
-}) {
-  const thumb = useRef()
-  const { width: windowWidth, height: windowHeight } = useWindowSize()
-  const lenis = useLenis()
+export function Scrollbar() {
+  const thumbRef = useRef()
   const [innerMeasureRef, { height: innerHeight }] = useRect()
   const [thumbMeasureRef, { height: thumbHeight }] = useRect()
 
-  useLenis(
+  const lenis = useLenis(
     ({ scroll, limit }) => {
       const progress = scroll / limit
 
-      thumb.current.style.transform = `translate3d(0,${progress * (innerHeight - thumbHeight)}px,0)`
+      thumbRef.current.style.transform = `translate3d(0,${
+        progress * (innerHeight - thumbHeight)
+      }px,0)`
     },
     [innerHeight, thumbHeight],
   )
 
-  const [clicked, setClicked] = useState(false)
-
   useEffect(() => {
-    if (!clicked) return
+    let start = null
 
     function onPointerMove(e) {
+      if (!start) return
       e.preventDefault()
 
-      const offset = (windowHeight - innerHeight) / 2
-      const y = mapRange(0, windowHeight, e.clientY, -offset, innerHeight + offset)
+      const scroll = mapRange(
+        start,
+        innerHeight - (thumbHeight - start),
+        e.clientY,
+        0,
+        lenis.limit,
+      )
+      lenis.scrollTo(scroll, { immediate: true })
+    }
 
-      const progress = clamp(0, y / innerHeight, 1)
-      const newPos = lenis.limit * progress
-
-      lenis.isHorizontal ? window.scrollTo(newPos, 0) : window.scrollTo(0, newPos)
+    function onPointerDown(e) {
+      start = e.offsetY
     }
 
     function onPointerUp() {
-      setClicked(false)
+      start = null
     }
 
+    thumbRef.current?.addEventListener('pointerdown', onPointerDown, false)
     window.addEventListener('pointermove', onPointerMove, false)
     window.addEventListener('pointerup', onPointerUp, false)
 
     return () => {
+      thumbRef.current?.removeEventListener('pointerdown', onPointerDown, false)
       window.removeEventListener('pointermove', onPointerMove, false)
       window.removeEventListener('pointerup', onPointerUp, false)
     }
-  }, [clicked, windowHeight, windowWidth, lenis, innerHeight])
+  }, [lenis, innerHeight, thumbHeight])
 
   return (
-    <div
-      className={s.scrollbar}
-      style={{
-        '--color': theme.color,
-        '--border': theme.border,
-        '--min-height': theme.minHeight,
-        '--position-right': theme.positionRight,
-        '--border-size': theme.borderSize,
-        '--border-radius': theme.borderRadius,
-      }}
-    >
-      <div ref={innerMeasureRef} className={s.inner}>
-        <div
-          className={s.thumb}
-          ref={(node) => {
-            thumb.current = node
-            thumbMeasureRef(node)
-          }}
-          onPointerDown={() => {
-            setClicked(true)
-          }}
-        />
-      </div>
+      <div className={s.scrollbar}>
+        <div ref={innerMeasureRef} className={s.inner}>
+          <div
+            className={s.thumb}
+            ref={(node) => {
+              thumbRef.current = node
+              thumbMeasureRef(node)
+            }}
+          />
+        </div>
     </div>
   )
 }
